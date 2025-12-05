6e99c7749f40887e11f2d225eca8c5aad5af5690
unknown <blackskiesdancer@gmail.com>
2025-12-05 18:59:58 +1100

    created docs folder

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/firefly-mentoring/',   // 👈 add this line
+  base: '/firefly-mentoring/', 
 })
